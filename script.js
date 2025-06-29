1b26e13bf6167866313bafd1ee43cc27d717fb6e
heyitsdirra <auroraramadhani49@gmail.com>
2025-06-29 11:37:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,22 +13,17 @@ function showPage(pageId) {
 function cekToken() {
     let token = document.getElementById("tokenInput").value.trim().toLowerCase();
 
-    fetch("surat.json?v=" + Date.now())
-    .then(response => {
-        console.log("Status:", response.status);
-        if (!response.ok) throw new Error("HTTP error " + response.status);
-        return response.json();
-    })
-    .then(data => {
-        console.log("Data surat loaded:", data); // Tambahin ini!
-        if (data[token]) {
-            document.getElementById("isiSurat").innerText = data[token];
-            showPage("letter-page");
-        } else {
-            alert("Invalid Code! Try Again.");
-        }
-    })
-    .catch(error => console.error("Error loading letter data:", error));
+    fetch("surat.json")
+        .then(response => response.json())
+        .then(data => {
+            if (data[token]) {
+                document.getElementById("isiSurat").innerText = data[token];
+                showPage("letter-page");
+            } else {
+                alert("Invalid Code! Try Again.");
+            }
+        })
+        .catch(error => console.error("Error loading letter data:", error));
 }
 
 // Kembali ke Halaman Awal
